f71b629a518742400d59d94e7297108766d55ca5
BattleWoofer <omeet100@gmail.com>
2021-08-03 15:21:46 -0500

    l

---
+++ js/tp.js
@@ -0,0 +1,9 @@
+function updateTime(){
+    var time = new Date();
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds()
+
+    console.log(seconds)
+}
